143540fdc35257fefb1bd39723ca661cfbc03b4c
sophrs <sophia.shlomo@gmail.com>
2018-11-20 10:55:25 +0000

    First commit

---
+++ src/helloWorldassignment/HelloWorldassignment.java
@@ -0,0 +1,11 @@
+package helloWorldassignment;
+
+public class HelloWorldassignment {
+	
+	public static void main(String [] args) {
+		
+		String Hello = "Hello World!";
+		System.out.println(Hello);
+	}
+
+}
